63d4beb44c23ea217f184f611050547048274b55
Ralph EFfting <ralfting@gmail.com>
2017-12-04 10:01:33 -0200

    Fix styleguide es6

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const sum = (numberA, numberB) => numberA + numberB;
 const sub = (numberA, numberB) => numberA - numberB;
 const mult = (numberA, numberB) => numberA * numberB;
-const div = (numberA, numberB) => (numberB === 0) ? 'Não é possivel fazer divisão por zero!' : numberA / numberB;
+const div = (numberA, numberB) => ((numberB === 0) ? 'Não é possivel fazer divisão por zero!' : numberA / numberB);
 
 export {
   sum,
